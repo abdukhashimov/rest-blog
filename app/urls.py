b9f7786d1181685bea9f36f6eb29754823ca8e76
Madiyor <abdukhashimov@yandex.ru>
2020-02-15 23:45:34 +0500

    jwt urls added

--- app/urls.py
+++ app/urls.py
@@ -3,7 +3,22 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 
+# jwt urls
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView
+)
+# end of jwt urls
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/token/', TokenObtainPairView.as_view(),
+         name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(),
+         name='token_refresh'),
+    path('api/token/verify/', TokenVerifyView.as_view(),
+         name='token_verify'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) +\
     static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
